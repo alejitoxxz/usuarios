9599ed62c0076ce4e30846fb8c112b40c182f91d
Jose Alejandro Valencia Henao <jose.valencia1373@uco.net.co>
2026-03-07 03:07:11 -0500

    Primer commit del microservicio de login

---
+++ src/main/java/co/edu/uco/login/LoginEatupApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginEatupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginEatupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uco/login/LoginEatupApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginEatupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
