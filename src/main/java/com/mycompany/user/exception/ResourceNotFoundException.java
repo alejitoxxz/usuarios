e3e2e2ea115f64e8b857371a68fe7b235d0c12af
Jose Alejandro Valencia <josevalenciahenao6@gmail.com>
2026-03-14 11:41:13 -0500

    Remove legacy com.mycompany.user module and scan path

--- src/main/java/com/mycompany/user/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.user.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
